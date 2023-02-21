8299823a3c7d22d250bac2334c959f99a9382592
Nicola Salzano <nicolasalzano2913@gmail.com>
2023-02-21 02:28:45 +0100

    add swiper and collection section

---
+++ index.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerview: 3,
+    spaceBetween: 80,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 5,
+        },
+      }
+    
+  });
